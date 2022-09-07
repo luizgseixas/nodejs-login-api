1ba390b5599aebdd35c52cf6587f37afb0e0fbc5
Luiz_Seixas <ticoluizgustavo@outlook.com>
2022-09-06 21:19:21 -0300

    move requests example to a separade file

--- src/examples/routes-example.ts
+++ src/examples/routes-example.ts
@@ -7,6 +7,22 @@ server.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Hello World!' })
 })
 
+server.get('/batatinha', (req: Request, res: Response) => {
+  const qr = req.query
+
+  console.log(qr)
+
+  res.json({ message: 'Aqui sua batatinha' })
+})
+
+server.get('/batatinha/:id', (req: Request, res: Response) => {
+  const param = req.params
+
+  console.log(param)
+
+  res.json({ message: 'Aqui sua batatinha' })
+})
+
 server.post('/batatinha', (req: Request, res: Response) => {
   const bd = req.body
 
